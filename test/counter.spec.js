36d6bd56af439fe1453bf3a77b1e73fd38111666
mannuelf <mannuel.ferreira@gmail.com>
2017-12-14 13:56:31 +0200

    trigger click on button

--- test/counter.spec.js
+++ test/counter.spec.js
@@ -1,15 +1,21 @@
+
 import { mount } from 'vue-test-utils';
 import Counter from '../src/Components/Counter.js'; 
 import expect from 'expect';
 
 describe ('Counter', () => {
+    
+    let wrapper = mount(Counter); // mount the counter wrapper
+    
+    beforeEach(() => {
+        wrapper = mount(Counter);
+    });
 
     it ('defaults to a count of 0', () => {
-        let wrapper = mount(Counter); // mount the counter wrapper
         wrapper.vm.count // access the data properties on the vm
     });
 
-    it ('increments the count when the button is clicked', () => {
+    it.only ('increments the count when the button is clicked', () => {
         expect(wrapper.vm.count).toBe(0);
         wrapper.find('button').trigger('click');
         expect(wrapper.vm.count).toBe(1);
@@ -17,6 +23,8 @@ describe ('Counter', () => {
 
     it ('presents the current count', () => {
         expect(wrapper.find('.count').html().toContain(0));
+        wrapper.find('button').trigger('click');
+        expect(wrapper.find('.count').html().toContain(1));
     });
 
 });
